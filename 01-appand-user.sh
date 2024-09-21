05700f0b2da5861da7e045199a02436430f17890
shivani <shiva.indla1519@gmail.com>
2024-09-21 22:05:03 +0530

    shell-ansible

--- 01-appand-user.sh
+++ 01-appand-user.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
-USERS="abc"
+USERS=$@
 
 for i in $USERS
 do
-    echo "$i"
+    cd /root/git-repose/ansible
+    echo "user: '$i'" > users.yml
+    ansible-playbook -i inventory.ini -e ansible_user=azure-user -e ansible_password=Azure@123456 02-ansible.yml
 done
